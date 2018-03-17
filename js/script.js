a5b3cf81a071b26efe3f0cc855ea6d1c2acb62e3
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-17 22:24:42 +0100

    task 11.4 finished

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function Phone(brand, price, color, screenSize){
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+  this.screenSize = screenSize;
+}
+Phone.prototype.printInfo = function() {
+  console.log("The phone brand is " + this.brand + ", the color is " + this.color + " and price is " + this.price + "PLN. The screen size is " + this.screenSize + '".');
+}
+Phone.prototype.buy = function() {
+  if (confirm("Would you like to buy " + this.brand + "?")){
+    alert("Congratulation you choose " + this.brand + ".");
+  }
+  else {
+    alert("Choose another phone.");
+  }
+}
+var iPhone6S = new Phone("Apple", 2250, "white", 5.5);
+var SamsungGalaxyS6 = new Phone("Samsung", 1870, "graphite", 5);
+var OnePlusOne = new Phone("OnePlus", 1500, "black", 4.5);
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
+iPhone6S.buy();
+SamsungGalaxyS6.buy();
+OnePlusOne.buy();
